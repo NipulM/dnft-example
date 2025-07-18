101cb727a94bb646be9032d707dee3f278b9b11e
Nipul Mallikarachchi <nipulm03@gmail.com>
2025-07-18 11:26:04 +0530

    impr: swtiched to base sepolia

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -6,13 +6,20 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    //   accounts: [process.env.PRIVATE_KEY!],
+    // },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
       accounts: [process.env.PRIVATE_KEY!],
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    // apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      baseSepolia: process.env.BASESCAN_API_KEY!,
+    },
   },
 };
 
